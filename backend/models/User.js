49b82ae5e1d5390f74f1ba681b400ab4e4725dcd
madupojumounika <madupojumounika0@gmail.com>
2026-02-10 15:21:03 +0530

    Update User model schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,25 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+
+    problemsSolved: { type: Number, default: 0 },
+    totalScore: { type: Number, default: 0 },
+    avgScore: { type: Number, default: 0 },
+
+    skills: [
+      {
+        name: String,
+        score: Number,
+      },
+    ],
+
+    recentActivity: [String],
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
